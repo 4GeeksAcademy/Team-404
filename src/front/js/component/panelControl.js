43312e0bf53f0e7935c3aba2cf365efd1c1d0f22
Víctor Bravo <vbravo.quintana@gmail.com>
2024-09-30 21:45:27 +0000

    panelControl

---
+++ src/front/js/component/panelControl.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkedAlt, faHome, faTruck, faUserTie, faUsers, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+const ControlPanel = ({ onLogout }) => {
+    return (
+        <div className="profile-control-panel">
+            <h1>Panel de Control</h1>
+            <ul>
+                <li>
+                    <Link to="/Mapa">
+                        <FontAwesomeIcon icon={faMapMarkedAlt} /> Planner (Ruta)
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/direcciones">
+                        <FontAwesomeIcon icon={faHome} /> Mis Direcciones
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/flota">
+                        <FontAwesomeIcon icon={faTruck} /> Vehículos y Conductores
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/autonomos">
+                        <FontAwesomeIcon icon={faUserTie} /> Autónomos
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/clientes">
+                        <FontAwesomeIcon icon={faUsers} /> Clientes
+                    </Link>
+                </li>
+            </ul>
+            <button onClick={onLogout} className="logout-button">
+                <FontAwesomeIcon icon={faSignOutAlt} /> Cerrar Sesión
+            </button>
+        </div>
+    );
+};
+
+export default ControlPanel;
